f2f603d02146a6e151874a3b898708e760df1c25
RAJEEV ANAND <rajeevanand840@gmail.com>
2021-12-11 12:41:11 +0530

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -75,9 +75,70 @@ clf.fit(X_train, y_train)
 yhat = clf.predict(X_test)
 yhat [0:5]
 
-<h2 id="evaluation">Evaluation</h2>
+# Evaluation 
+from sklearn.metrics import classification_report, confusion_matrix as cm
+import itertools
 
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
 
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_test, yhat, labels=[2,4])
+np.set_printoptions(precision=2)
+
+print (classification_report(y_test, yhat))
+
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=['Benign(2)','Malignant(4)'],normalize= False,  title='Confusion matrix')
+
+# You can also easily use the f1_score from sklearn library:
+from sklearn.metrics import f1_score
+f1_score(y_test, yhat, average='weighted') 
+
+# Let's try the jaccard index for accuracy:
+from sklearn.metrics import jaccard_score
+jaccard_score(y_test, yhat,pos_label=2)
+
+# Practice
+# Can you rebuild the model, but this time with a __linear__ kernel? You can use __kernel='linear'__ option, when you define the svm. How the accuracy changes with the new kernel function?
+# write your code here
+clf2 = svm.SVC(kernel='linear')
+clf2.fit(X_train, y_train) 
+yhat2 = clf2.predict(X_test)
+print("Avg F1-score: %.4f" % f1_score(y_test, yhat2, average='weighted'))
+print("Jaccard score: %.4f" % jaccard_score(y_test, yhat2,pos_label=2))
 
 
 
